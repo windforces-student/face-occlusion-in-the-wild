8185291e8ed7bb329343a33a735501ccb1e495b7
henderson <henderson.park@mathpresso.com>
2022-08-10 20:42:57 +0900

    First commit

---
+++ suppressor.py
@@ -0,0 +1,16 @@
+import sys
+
+
+class Suppressor(object):
+
+    def __enter__(self):
+        self.stdout = sys.stdout
+        sys.stdout = self
+
+    def __exit__(self, type, value, traceback):
+        sys.stdout = self.stdout
+        if type is not None:
+
+    # Do normal exception handling
+    def write(self, x):
+        pass
